dfb7bf4bd74ef7f2e5552438a8bf68c1eb80af08
nickoro094 <32685325+nickoro094@users.noreply.github.com>
2019-06-25 20:35:23 -0500

    Add files via upload

--- FL+BP+ES/Software/ExpertSystem.py
+++ FL+BP+ES/Software/ExpertSystem.py
@@ -3,9 +3,9 @@ from pyDatalog import pyDatalog
 pyDatalog.create_terms('Riesgo, vel, Aviso')
 
 
-+Riesgo(0,"70-90")
-+Riesgo(1,"50-60")
-+Riesgo(2,"30-40")
++Riesgo(0,"45-90")
++Riesgo(1,"30-60")
++Riesgo(2,"20-40")
 +Riesgo(3,"10-30")
 
 V(Riesgo,Aviso) <= vel(Riesgo, Aviso)
